4aba2ad51413a203ff5575d124175415087db7ac
WilliamHeuser <sdr366@alumni.ku.dk>
2022-06-09 16:50:14 +0200

    More cleaning

--- fill_tables_mac.sql
+++
@@ -1,6 +0,0 @@
-\copy gdp from '/Users/ap/Dropbox/KU/4. Semester/Databases and Informationssystems/Project/DIS-Project/gdp_data/gdp.csv' csv header;
-\copy gdp_growth from '/Users/ap/Dropbox/KU/4. Semester/Databases and Informationssystems/Project/DIS-Project/gdp_data/gdp_growth.csv' csv header;
-\copy gdp_per_capita from '/Users/ap/Dropbox/KU/4. Semester/Databases and Informationssystems/Project/DIS-Project/gdp_data/gdp_per_capita.csv' csv header;
-\copy gdp_per_capita_growth from '/Users/ap/Dropbox/KU/4. Semester/Databases and Informationssystems/Project/DIS-Project/gdp_data/gdp_per_capita_growth.csv' csv header;
-\copy gdp_ppp from '/Users/ap/Dropbox/KU/4. Semester/Databases and Informationssystems/Project/DIS-Project/gdp_data/gdp_ppp.csv' csv header;
-\copy gdp_ppp_per_capita from '/Users/ap/Dropbox/KU/4. Semester/Databases and Informationssystems/Project/DIS-Project/gdp_data/gdp_ppp_per_capita.csv' csv header;
